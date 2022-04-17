be04d6542c39874dbbe6b59f36382dc3a55ab64b
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2022-04-17 06:35:05 +0430

    game_controller.py added.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import sys
+from hive_game import HiveGame
+from game_controller import GameController
 
 
+def main():
+    game_controller = GameController()
+    game_controller.start()
 
+
+if __name__ == '__main__':
+    sys.exit(main())
 
--- model/pieces/beetle.py
+++
@@ -1,8 +0,0 @@
-from model.pieces.pices import Pieces
-
-class Beetle(Pieces):
-    # def __init__(self, color, role, num):
-    #     super().__init__(color, role, num)
-
-    def permitted_movements(self, location):
-        pass
